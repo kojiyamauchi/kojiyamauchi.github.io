71c0df344f32b541c7b2c95f1cc50940e473e89c
kojiyamauchi <all.the.small.things@me.com>
2020-10-01 20:37:29 +0900

    update around components.

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -82,7 +82,7 @@ export const SEO: React.FC<Props> = ({ lang = 'ja', meta = [], ...props }): JSX.
       <link
         href="https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Source+Sans+Pro:wght@300;400;700;900&display=swap"
         rel="stylesheet"
-      ></link>
+      />
     </Helmet>
   )
 }
