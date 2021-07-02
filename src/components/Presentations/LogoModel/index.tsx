c7d6abbc49cc79592be46aca6a27f766c785429c
kojiyamauchi <all.the.small.things@me.com>
2021-07-02 22:15:32 +0900

    fix.

--- src/components/Presentations/LogoModel/index.tsx
+++ src/components/Presentations/LogoModel/index.tsx
@@ -3,6 +3,5 @@
 */
 
 import dynamic from 'next/dynamic'
-// import { Props } from './Component'
 
 export const DynamicComponentLogoModel = dynamic<unknown>(() => import('./Component').then((component) => component.LogoModel), { ssr: false })
