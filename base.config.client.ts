155fbbf7802c9b4916657fb361fe2e681ec44008
kojiyamauchi <kojiyamauchi@users.noreply.github.com>
2022-08-20 14:01:31 +0000

    deploy: 49e4212c45fdcf3da433321b39b7aa770c4a6d36

--- base.config.client.ts
+++ base.config.client.ts
@@ -4,10 +4,10 @@
 
 export const baseSeo = {
   title: 'Re: All The Small Things, | Koji Yamauchi',
-  description: `I'm Freelance Web Developer, Based in Tokyo, Japan. My skills, TypeScript / JavaScript / React / Next.js / Webpack / Gulp / Node.js / GitHub / Sass / CSS / HTML / VSCode / Figma(a little) / Sketch(a little) / Photoshop / Illustrator. from Koji Yamauchi`,
+  description: `I'm Freelance Web Developer, Based in Tokyo, Japan. My skills, TypeScript / JavaScript / React / Next.js / GraphQL / Node.js / Webpack / Gulp / GitHub / GitHub Actions / Sass / CSS / HTML / VSCode / Photoshop / Illustrator. from Koji Yamauchi`,
   location: 'https://kojiyamauchi.github.io/',
   ogpSiteName: 'Re: All The Small Things, | kojiyamauchi.github.io',
   ogpTitle: 'Re: All The Small Things, | kojiyamauchi.github.io',
-  ogpDescription: `I'm Freelance Web Developer, Based in Tokyo, Japan. My skills, TypeScript / JavaScript / React / Next.js / Webpack / Gulp / Node.js / GitHub / Sass / CSS / HTML / VSCode / Figma(a little) / Sketch(a little) / Photoshop / Illustrator. from Koji Yamauchi`,
+  ogpDescription: `I'm Freelance Web Developer, Based in Tokyo, Japan. My skills, TypeScript / JavaScript / React / Next.js / GraphQL / Node.js / Webpack / Gulp / GitHub / GitHub Actions / Sass / CSS / HTML / VSCode / Photoshop / Illustrator. from Koji Yamauchi`,
   ogpImage: 'ogp_image.jpg'
 } as const
